1e2b0579c49bc6f955d02960eaeed9705d7f9297
sKawashima <bysk.333@gmail.com>
2015-07-09 21:06:22 +0900

    fix mobile

--- js/scroll_show.js
+++ js/scroll_show.js
@@ -2,10 +2,12 @@ $(function() {
 	$(window).scroll(function () {
 		var s = $(this).scrollTop();
 		var m = $(window).height()-65;
-		if(s > m) {
-			$("#nav").fadeIn('slow');
-		} else {
-			$("#nav").fadeOut('slow');
+		if($(window).width() > 768){
+			if(s > m) {
+				$("#nav").fadeIn('slow');
+			} else {
+				$("#nav").fadeOut('slow');
+			}
 		}
 	});
 });
